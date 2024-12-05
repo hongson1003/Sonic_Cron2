bf60b9e4f55207ca2c32175a46c453037f070d2f
hongsonit10 <hongsonit10@gmail.com>
2024-12-05 10:56:19 +0700

    add cronB

---
+++ index.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
+const cron = require("node-cron");
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// API để ping Cron A
+const pingCronA = async () => {
+  try {
+    await axios.get(process.env.CRON_A_URL);
+    console.log(`[${new Date().toISOString()}] Pinged Cron A successfully!`);
+  } catch (error) {
+    console.error(
+      `[${new Date().toISOString()}] Failed to ping Cron A:`,
+      error.message
+    );
+  }
+};
+
+// Cài đặt cron job để gọi Cron A mỗi 3 phút
+cron.schedule("*/3 * * * *", async () => {
+  console.log(
+    `[${new Date().toISOString()}] Running cron job to ping Cron A...`
+  );
+  await pingCronA();
+});
+
+// Route để nhận yêu cầu từ Cron A
+app.get("/ping-from-crona", async (req, res) => {
+  console.log(
+    `[${new Date().toISOString()}] Received ping from Cron A, responding...`
+  );
+  res.status(200).send("Received ping from Cron A successfully!");
+});
+
+// Bắt đầu server Express
+app.listen(PORT, () => {
+  console.log(`Cron B is running on http://localhost:${PORT}`);
+});
