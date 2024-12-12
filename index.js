e9258656d11da4e58ae68e99c9c3b5b7457e068e
phamvankhoa <phamvankhoa@gmail.com>
2024-12-12 19:04:33 +0700

    add to string

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ const PORT = process.env.PORT || 4000;
 const pingCronA = async () => {
   try {
     await axios.get(process.env.CRON_A_URL);
-    console.log(`[${new Date().toISOString()}] Pinged Cron A successfully!`);
+    console.log(`[${new Date().toString()}] Pinged Cron A successfully!`);
   } catch (error) {
     console.error(
-      `[${new Date().toISOString()}] Failed to ping Cron A:`,
+      `[${new Date().toString()}] Failed to ping Cron A:`,
       error.message
     );
   }
@@ -21,16 +21,14 @@ const pingCronA = async () => {
 
 // Cài đặt cron job để gọi Cron A mỗi phút
 cron.schedule("* * * * *", async () => {
-  console.log(
-    `[${new Date().toISOString()}] Running cron job to ping Cron A...`
-  );
+  console.log(`[${new Date().toString()}] Running cron job to ping Cron A...`);
   await pingCronA();
 });
 
 // Route để nhận yêu cầu từ Cron A
 app.get("/ping-from-crona", async (req, res) => {
   console.log(
-    `[${new Date().toISOString()}] Received ping from Cron A, responding...`
+    `[${new Date().toString()}] Received ping from Cron A, responding...`
   );
   res.status(200).send("Received ping from Cron A successfully!");
 });
